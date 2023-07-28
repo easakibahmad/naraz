d0f25cf18b5bfd6932327200764ab58fb209c9c6
easakibahmad <sakib.sgc.du@gmail.com>
2023-07-28 19:41:49 +0600

    navbar, carousel initial setup

---
+++ src/components/Card1.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Card1 = () => {
+  return (
+    <div className="grid grid-rows-4 grid-cols-1">
+      <div className=" row-span-3 bg-yellow-400"></div>
+      <div className="bg-white"></div>
+    </div>
+  );
+};
+
+export default Card1;
---
+++ src/components/HomeCarousel2.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const HomeCarousel2 = () => {
+  return <div></div>;
+};
+
+export default HomeCarousel2;
